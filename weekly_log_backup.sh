5f8e6d6edfae3a8f99ccc8fa45faab0603c1487a
bit-dibya <dibyajyoti.giri1302@gmail.com>
2025-11-16 16:09:06 +0000

    added backup feature from LOGDIR to BACKUPDIR

--- weekly_log_backup.sh
+++ weekly_log_backup.sh
@@ -6,3 +6,11 @@ BACKUPDIR="${2:-/var/backups/logs}"
 RETENTION_DAYS=7
 TIMESTAMP=$(date +%F_%H%M%S)
 OUTFILE="$BACKUPDIR/logs_backup_$TIMESTAMP.tar.gz"
+
+#creating directory for backup file if it doesn't exists
+mkdir -p "$BACKUPDIR"
+
+#going into the log directory and create a gzipped tar of everything and save as OUTFILE
+tar -czf "$OUTFILE" -C "$LOGDIR"
+
+

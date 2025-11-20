ea94fdd01a54b9c9a8acf60c5e8e2b27ea272f5d
bit-dibya <dibyajyoti.giri1302@gmail.com>
2025-11-20 19:26:10 +0530

    adding comments to system healthcheck script

--- sys_hc.sh
+++ sys_hc.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
+#defingng output log path
 OUTLOG=/home/dibyagiri/syslogs/system_health.log
 
+#defingin thersholds for cpu,memory and disk utilisation
 CPU_THRESH=80
 MEM_THRESH=80
 DISK_THRESH=85
+
 
 timestamp() {
   date -Iseconds
 }
 
-CPU=$(top -bn1 | awk -F'[, ]+' '/Cpu\(s\):/ {print 100 - $8}')
-MEM=$(free | awk '/Mem:/ {print $3/$2 * 100}')
-DISK=$(df / | awk 'END{print $5}' | tr -d '%')
+CPU=$(top -bn1 | awk -F'[, ]+' '/Cpu\(s\):/ {print 100 - $8}')    #extracting cpu utilisation from top
+MEM=$(free | awk '/Mem:/ {print $3/$2 * 100}')    #extracting memory utilisation from free
+DISK=$(df / | awk 'END{print $5}' | tr -d '%')    #extracting disk utilisation from df
 
+#printing the utilisations
 echo "$(timestamp) CPU:${CPU}% MEM:${MEM}% DISK:${DISK}%" >> "$OUTLOG"
 
+#setting up alert
 awk -v c="$CPU" -v m="$MEM" -v d="$DISK" -v t="$(timestamp)" \
     -v cpu_t="$CPU_THRESH" -v mem_t="$MEM_THRESH" -v disk_t="$DISK_THRESH" '
 BEGIN {

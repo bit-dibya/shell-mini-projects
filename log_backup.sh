01f9d293da47ab190890ef1ca88aeb23021959bc
bit-dibya <dibyajyoti.giri1302@gmail.com>
2025-11-30 16:54:55 +0530

    minor modifications

--- log_backup.sh
+++ log_backup.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 #declaring the variables
-LOGDIR="${1:-/var/log}"
-BACKUPDIR="${2:-/var/backups/logs}"
-RETENTION_DAYS=7
+LOGDIR=/home/dibyagiri/syslogs
+BACKUPDIR=/home/dibyagiri/syslogs/backup
+RETENTION_DAYS=15
 TIMESTAMP=$(date +%F_%H%M%S)
-OUTFILE="$BACKUPDIR/logs_backup_$TIMESTAMP.tar.gz"
 
 #creating directory for backup file if it doesn't exists
 mkdir -p "$BACKUPDIR"
+OUTFILE="$BACKUPDIR/logs_backup_$TIMESTAMP.tar.gz"
 
-#going into the log directory and create a gzipped tar of everything and save as OUTFILE
-tar -czf "$OUTFILE" -C "$LOGDIR"
-
-#logging
-if [ $? -eq 0 ]; then
-	echo "$(date -Iseconds) Backup OK: $OUTFILE" >> /var/log/automation/log_backup.log
-else
-	echo "$(date -Iseconds) Backup FAIL" >> /var/log/automation/log_backup.log
-	exit 1
-fi
+#going into the log directory and create a gzipped tar of all the log files and save as OUTFILE
+cd "$LOGDIR"
+tar -czf "$OUTFILE" ./*.log
 
 #removing backups older than 15 days 
 find "$BACKUPDIR" -type f -name 'logs_backup_*.tar.gz' -mtime +"$RETENTION_DAYS" -exec rm -f {} \;
